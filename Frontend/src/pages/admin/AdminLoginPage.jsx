51cd30760f7d292dd0635b6dbccc377ad47d0c13
neeldhoble <neeldhoble2004@gmail.com>
2025-06-24 14:36:12 +0530

    Initial commit

---
+++ Frontend/src/pages/admin/AdminLoginPage.jsx
@@ -0,0 +1,7 @@
+const AdminLoginPage = () => (
+  <div className="p-6 max-w-xl mx-auto">
+    <h2 className="text-2xl font-semibold mb-4">Checkout</h2>
+    {/* Form to enter user info like name, phone, address */}
+  </div>
+);
+export default AdminLoginPage;
